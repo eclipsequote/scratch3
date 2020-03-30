d439f9d3c360aae0c1cc616ef4b05ac5d14633ad
eclipsequote <eclipsequote@163.com>
2020-03-31 01:28:27 +0800

    init commit

---
+++ scratch-desktop/scripts/lib/libraries.js
@@ -0,0 +1,13 @@
+const backdrops = require('scratch-gui/dist/libraries/backdrops.json');
+const costumes = require('scratch-gui/dist/libraries/costumes.json');
+const sounds = require('scratch-gui/dist/libraries/sounds.json');
+const sprites = require('scratch-gui/dist/libraries/sprites.json');
+
+const libraries = {
+    backdrops,
+    costumes,
+    sounds,
+    sprites
+};
+
+module.exports = libraries;
